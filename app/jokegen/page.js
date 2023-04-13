c95495bdc12294811a9cc4eb0840efc87fe7fbe7
Phantom <sambaeverything@gmail.com>
2023-04-13 18:05:47 +0200

    loading with errors

--- app/jokegen/page.js
+++ app/jokegen/page.js
@@ -4,7 +4,7 @@ import Image from 'next/image'
 
 
 
-export default function Home() {
+export default function Home2() {
   const handleSubmit = (event) => {
     event.preventDefault(); // prevent page refresh
 
--- app/page.js
+++ app/page.js
@@ -1,28 +1,30 @@
-import Image from 'next/image'
-
-
-
-
+"use client";
+import Image from "next/image";
 
 export default function Home() {
   const handleSubmit = (event) => {
     event.preventDefault(); // prevent page refresh
 
-
-    console.log('this is your form submission');
+    console.log("this is your form submission");
   };
   return (
     <main>
-    <h1 className="text-4xl font-bold underline">
-      If Chuck Norris Was a ..
-    </h1>
-    <form onSubmit={handleSubmit}>
-        <input className='bg-gray-200 shadow-inner rounded-l p-2 flex-1' id='joke' type='text' aria-label='email address' placeholder='If Chuck Norris Was A ...' />
-        <button className='bg-blue-600 hover:bg-blue-700 duration-300 text-white shadow p-2 rounded-r' type='submit'>
+      <h1 className="text-4xl font-bold underline">If Chuck Norris Was a ..</h1>
+      <form onSubmit={handleSubmit}>
+        <input
+          className="bg-gray-200 shadow-inner rounded-l p-2 flex-1"
+          id="joke"
+          type="text"
+          aria-label="email address"
+          placeholder="If Chuck Norris Was A ..."
+        />
+        <button
+          className="bg-blue-600 hover:bg-blue-700 duration-300 text-white shadow p-2 rounded-r"
+          type="submit"
+        >
           Generate Joke
         </button>
       </form>
-   
     </main>
-  )
+  );
 }
